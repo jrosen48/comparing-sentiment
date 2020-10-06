e6302e54ade29e12e513a9c379d030a2cb7df3d7
Joshua Rosenberg <jmrosen48@gmail.com>
2020-10-05 20:14:07 -0400

    setup targets

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 comparing-sentiment
+
+## Using {targets}
+
+This project uses {targets} for workflow management. To install {targets}:
+
+```{r}
+library(remotes)
+install_github("wlandau/targets")
+```
+
+To run the project pipeline, enter and run the following function:
+
+`targets::tar_make()`
+
+## Directory structure
+
+- Raw data: `data-raw`
+- Data that can be shared: `data`
+- Functions: `r/functions.R`
+
+## Study data
+
+Please contact the authors for access to the study data.
--- _targets.R
+++ _targets.R
@@ -3,27 +3,13 @@ library(targets)
 source(here::here("R", "functions.R"))
 
 # Set target-specific options such as packages.
-tar_option_set(packages = c("purrr", "readr", "ggplot2", "janitor", "dplyr",
-                            "here", "lubridate", "tidyr")
-)
+tar_option_set(packages = c("here", "tidyverse"))
 
 # Define targets
 targets <- list(
-  tar_target(files_ccss, list.files(here::here("data", "ccss"), 
-                               full.names = TRUE,
-                               pattern = "\\.csv$", 
-                               recursive = TRUE), # if data is organized by sub-directories; w/ all data, can take awhile
-             format = "file"),
-  tar_target(raw_data_ccss, map_df(files_ccss, read_csv_with_col_types)),
-  tar_target(d_ccss, prep_data(raw_data_ccss)),
   
-  tar_target(files_ngss, list.files(here::here("data", "ngss"), 
-                                    full.names = TRUE,
-                                    pattern = "\\.csv$", 
-                                    recursive = TRUE), # if data is organized by sub-directories; w/ all data, can take awhile
-             format = "file"),
-  tar_target(raw_data_ngss, map_df(files_ngss, read_csv_with_col_types)),
-  tar_target(d_ngss, prep_data(raw_data_ngss))
+  tar_target(test_target, test_f),
+  tar_target(another_test_target, test_f)
   
 )
 
