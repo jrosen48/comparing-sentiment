8f3930da8d5429da3020a783173c53ebe6f477f3
conradborchers <58824414+conradborchers@users.noreply.github.com>
2020-10-13 14:49:50 +0200

    MARKED: commented out teacher functions, will delete in next commit

--- r/functions.R
+++ r/functions.R
@@ -35,27 +35,27 @@ add_liwc <- function(d, liwc_data){
   return(d)
 }
 
-add_teacher_classification <- function(d, teacher_classification_data){
-  d$is_teacher <- teacher_classification_data[,1]
-  # assign role at random for ambiguous classification due to changed profiles over time
-  # seed automatically determined by targets
-  dups <- d %>% 
-            select(c("user_id", "is_teacher")) %>%
-            unique() 
-  dups <- dups[duplicated(dups$user_id),] 
-  dups$is_teacher <- sample(0:1, nrow(dups), replace=T)
-  h <- hash(dups$user_id, dups$is_teacher)
-  d$is_teacher[d$user_id %in% keys(h)] <- sapply(d$user_id[d$user_id %in% keys(h)], 
-                                                 FUN=function(k){h[k] %>% values() %>% as.numeric()})
-  return(d)
-}
+#add_teacher_classification <- function(d, teacher_classification_data){
+#  d$is_teacher <- teacher_classification_data[,1]
+#  # assign role at random for ambiguous classification due to changed profiles over time
+#  # seed automatically determined by targets
+#  dups <- d %>% 
+#            select(c("user_id", "is_teacher")) %>%
+#            unique() 
+#  dups <- dups[duplicated(dups$user_id),] 
+#  dups$is_teacher <- sample(0:1, nrow(dups), replace=T)
+#  h <- hash(dups$user_id, dups$is_teacher)
+#  d$is_teacher[d$user_id %in% keys(h)] <- sapply(d$user_id[d$user_id %in% keys(h)], 
+#                                                 FUN=function(k){h[k] %>% values() %>% as.numeric()})
+#  return(d)
+#}
 
-add_external_master <- function(d, ss_scale_data, ss_binary_data, liwc_data, teacher_classification_data){
+add_external_master <- function(d, ss_scale_data, ss_binary_data, liwc_data){ #, teacher_classification_data){
   return(
     d %>%
       add_sentistrength(ss_scale_data=ss_scale_data, ss_binary_data=ss_binary_data) %>%
-      add_liwc(liwc_data=liwc_data) %>%
-      add_teacher_classification(teacher_classification_data=teacher_classification_data)
+      add_liwc(liwc_data=liwc_data) # %>%
+      # add_teacher_classification(teacher_classification_data=teacher_classification_data)
   )
 }
 
@@ -66,7 +66,8 @@ remove_variables <- function(d){
     d %>% select(c("status_id", "user_id", "text", "created_at", "dl_at",
                    "is_retweet", "is_quote", "reply_to_status_id", "reply_to_user_id",
                    "favorite_count", "retweet_count", "quote_count", "reply_count", "lang", # tweet level
-                   "is_teacher", "followers_count", "friends_count", "listed_count", # user level
+                   #"is_teacher", 
+                   "followers_count", "friends_count", "listed_count", # user level
                    "ss_pos", "ss_neg", "ss_scale", "ss_scale_scaled", "ss_binary", # sentistrength sentiment
                    "liwc_pos", "liwc_neg", "liwc_scale", "liwc_scale_scaled", "liwc_binary")) # liwc sentiment
   )
@@ -522,20 +523,20 @@ ambiguity_context_by_method <- function(d){
   print(aggregate(tidytext_ambi ~ q, d, mean))
 }
 
-discrepancy_role_by_method <- function(d){
-  print("discrepancy_role_by_method")
-  print(aggregate(total_discrepancy %>% unlist() ~ is_teacher, d, mean))
-  print(aggregate(ss_liwc_discrepancy %>% unlist() ~ is_teacher, d, mean))
-  print(aggregate(ss_tidytext_discrepancy %>% unlist() ~ is_teacher, d, mean))
-  print(aggregate(liwc_tidytext_discrepancy %>% unlist() ~ is_teacher, d, mean))
-}
+#discrepancy_role_by_method <- function(d){
+#  print("discrepancy_role_by_method")
+#  print(aggregate(total_discrepancy %>% unlist() ~ is_teacher, d, mean))
+#  print(aggregate(ss_liwc_discrepancy %>% unlist() ~ is_teacher, d, mean))
+#  print(aggregate(ss_tidytext_discrepancy %>% unlist() ~ is_teacher, d, mean))
+#  print(aggregate(liwc_tidytext_discrepancy %>% unlist() ~ is_teacher, d, mean))
+#}
 
-ambiguity_role_by_method <- function(d){
-  print("ambiguity_role_by_method")
-  print(aggregate(ss_ambi ~ is_teacher, d, mean))
-  print(aggregate(liwc_ambi ~ is_teacher, d, mean))
-  print(aggregate(tidytext_ambi ~ is_teacher, d, mean))
-}
+#ambiguity_role_by_method <- function(d){
+#  print("ambiguity_role_by_method")
+#  print(aggregate(ss_ambi ~ is_teacher, d, mean))
+#  print(aggregate(liwc_ambi ~ is_teacher, d, mean))
+#  print(aggregate(tidytext_ambi ~ is_teacher, d, mean))
+#}
 
 descriptives_master <- function(d){
   d %>%
