26a25665603adc7fee58291bf403fd0b8468013f
Joshua Rosenberg <jmrosen48@gmail.com>
2020-10-30 17:12:27 -0400

    remove duplicate targets

--- _targets.R
+++ _targets.R
@@ -54,7 +54,7 @@ targets <- list(
   #tar_target(write_raw_data_combined_for_liwc, raw_data_combined %>% select(status_id, text) %>% write_csv(here("data-sentiment", "raw-data-combined-for-liwc.csv"))),
   ######
   
-  tar_target(raw_with_external, add_external_master(raw_data, ss_scale_data, ss_binary_data, teacher_class_data)),
+  #tar_target(raw_with_external, add_external_master(raw_data, ss_scale_data, ss_binary_data, teacher_class_data)),
 
   tar_target(clean_data_file, raw_with_external %>% clean_master(), format = "file"),
   tar_target(clean_data, readRDS(clean_data_file)),
@@ -78,25 +78,13 @@ targets <- list(
   tar_target(agree_df_states_1_20, access_manual_coding_data_state_data(1:20)),
   tar_target(agree_statistics_states_1_20, calculate_manual_agreement(agree_df_states_1_20)),
 
-  # manual coding reliability
-  tar_target(file_for_state_sample_for_qual_coding, here("data", "sample-of-state-tweets-for-qual-coding.csv")),
-  tar_target(joined_state_sample_for_qual_coding, join_raw_and_google_sheets_data(file_for_state_sample_for_qual_coding)),
-  
-  tar_target(agree_df_1_20, access_manual_coding_data(1:20)), # row indices are for the first 20 rows 
-  
-  tar_target(agree_statistics_1_20, calculate_manual_agreement(agree_df_1_20)),
-  tar_target(agree_df_21_45, access_manual_coding_data(21:45)),
-  tar_target(agree_statistics_21_45, calculate_manual_agreement(agree_df_21_45)),
-  tar_target(agree_df_states_1_20, access_manual_coding_data_state_data(1:20)),
-  tar_target(agree_statistics_states_1_20, calculate_manual_agreement(agree_df_states_1_20)),
-  
   # consensus codes from manual coding
-  tar_target(consensus_manual_codes, access_consensus_codes(1:45, 1:20)),
+  tar_target(consensus_manual_codes, access_consensus_codes(1:45, 1:20))
   
   # evaluate consensus
-  tar_target(consensus_with_software_ratings, combine_coding_and_software_ratings(consensus_manual_codes, final_data)),
+  #tar_target(consensus_with_software_ratings, combine_coding_and_software_ratings(consensus_manual_codes, final_data)),
   
-  tar_target(validation, consensus_with_software_ratings %>% validation_master)
+  #tar_target(validation, consensus_with_software_ratings %>% validation_master)
  
   # Descriptives and results in seperate Rmd Files in root folder for now
 
